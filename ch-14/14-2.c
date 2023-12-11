689b15da2332a3d4cec489bc5d32305359bc4c76
iamrinkalpipaliya <pipaliyarinkal0@gmail.com>
2023-12-11 19:48:28 +0530

    ch-14

---
+++ ch-14/14-2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+{
+	char k[20];
+	int i;
+	
+	
+	printf("enter the name : ");
+	scanf("%s",k);
+	
+	for(i=0;i<20;i++)
+	{
+		if(k[i]>=97 && k[i]<=122)
+		{
+				k[i] -=32;			
+		}
+	}
+	
+	printf("charcter :%s",k);
+		
+}
+
